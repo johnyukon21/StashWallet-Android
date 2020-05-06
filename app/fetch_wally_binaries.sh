22c6981ba7afd4607f7bbbea7ff439a5707fd70e
johnyukon21 <johnyukon21@gmail.com>
2020-05-06 11:12:59 -0700

    script for downloading and setting up Wally

--- app/fetch_wally_binaries.sh
+++ app/fetch_wally_binaries.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-# Downloads and installs the pre-built gdk libraries for use by Green-Android
+# Downloads and installs the pre-built wally libraries for use by StashWallet-Android
 set -e
 
-if [ -d gdk ]; then
-    echo "Found a 'gdk' folder, exiting now"
+if [ -d wallycore ]; then
+    echo "Found a 'wallycore' folder, exiting now"
     exit 0
 fi
 
-# The version of gdk to fetch and its sha256 checksum for integrity checking
-NAME="gdk-android-jni"
+# The version of wally to fetch and its sha256 checksum for integrity checking
+NAME="wallycore-android-jni"
 TARBALL="${NAME}.tar.gz"
-URL="https://github.com/Blockstream/gdk/releases/download/release_0.0.30/${TARBALL}"
-SHA256="a0b20f1b2a2da91c9a547b836fd2cfd4e761e53c095ad26b1379fc9cb4fdcbc6"
+URL="https://github.com/ElementsProject/libwally-core/releases/download/release_0.7.8/${TARBALL}"
+SHA256="809cc59e5f7d24197557f7fe5f5d8e274f68ee7037d6de5f68e437d5ae8385eb"
 # Pre-requisites
 function check_command() {
     command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
@@ -28,12 +28,12 @@ if [ -d "${APP_ROOT}/app" ]; then
 fi
 
 JNILIBSDIR=${APP_ROOT}/src/main/jniLibs
-GDK_JAVA_DIR="${APP_ROOT}/src/main/java/com/blockstream"
+WALLY_JAVA_DIR="${APP_ROOT}/src/main/java/com/blockstream"
 
 # Clean up any previous install
-rm -rf gdk-android-jni* ${APP_ROOT}/src/main/jniLibs ${GDK_JAVA_DIR}
+rm -rf wallycore-android-jni* ${APP_ROOT}/src/main/jniLibs ${WALLY_JAVA_DIR}
 
-# Fetch, validate and decompress gdk
+# Fetch, validate and decompress wally
 curl -sL -o ${TARBALL} "${URL}"
 echo "${SHA256}  ${TARBALL}" | shasum -a 256 --check
 tar xvf ${TARBALL}
@@ -41,7 +41,8 @@ rm ${TARBALL}
 
 # Move the libraries and Java wrapper where we need them
 mv ${NAME}/lib/ ${APP_ROOT}/src/main/jniLibs
-mv ${NAME}/java/com/blockstream/ ${GDK_JAVA_DIR}
+mkdir -p ${WALLY_JAVA_DIR}
+mv ${NAME}/src/swig_java/src/com/blockstream/* ${WALLY_JAVA_DIR}
 
 # Cleanup
 rm -fr $NAME
